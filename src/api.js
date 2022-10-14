11e06eb7cbcd57f1dec7273d5f2352b5eccb4046
Dario De Marco <47568450+DarioDemix@users.noreply.github.com>
2022-10-14 13:02:36 +0200

    Delete api.js

--- src/api.js
+++
@@ -1,8 +0,0 @@
-// This is a workaround needed for deploying the app as a web service
-// TODO: find a cleaner solution
-const express = require("express")
-const app = express()
-
-const startApiServer = () => app.listen(process.env.PORT, () => console.log(`Server started at port ${process.env.PORT}`))
-
-module.exports = startApiServer
